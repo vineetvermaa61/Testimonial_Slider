1ff8f52adc10376c3029849889bcf8e3577e54d2
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2024-06-29 20:19:03 +0530

    initial commit

---
+++ src/components/Testimonial.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import Card from './Card'
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
+
+const Testimonial = (props) => {
+    let reviews = props.reviews;
+    const[index, setIndex] = useState(0);
+
+    function leftShiftHandler(){
+        if(index-1 < 0){
+            setIndex(reviews.length-1);
+        }else{
+            setIndex(index-1);
+        }
+    }
+
+    function rightShiftHandler(){
+        if(index + 1 >= reviews.length){
+            setIndex(0);
+        }
+        else{
+            setIndex(index+1);
+        }
+    }
+
+    function surpriseHandler(){
+      let randomIndex = Math.floor(Math.random() * reviews.length);
+      setIndex(randomIndex);
+    }
+
+  return (
+    <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center 
+    mt-10 p-10 transition-all duration-200 hover:shadow-xl rounded-md'>
+        
+        <div><Card reviews = {reviews[index]} /></div>
+
+        {/* buttons yahan hone chahiye kyonki card mein toh wo data rakha hai jo change ho raha buttons ke click karne par */}
+        <div className='flex text-3xl mt-10 gap-3 text-violet-400 font-bold mx-auto'>
+            <button onClick={leftShiftHandler}
+            className='hover:text-violet-500 cursor-pointer'>
+                <FiChevronLeft/>
+            </button>
+            <button onClick={rightShiftHandler}
+            className='hover:text-violet-500 cursor-pointer'>
+                <FiChevronRight/>
+            </button>
+        </div>
+
+        <div className='mt-6'>
+            <button onClick={surpriseHandler}
+            className='bg-violet-400 hover:bg-violet-500 cursor-pointer transition-all duration-200 px-10 py-2
+            rounded-md font-bold text-lg text-white'>
+                Surprise Me
+            </button>
+        </div>
+
+    </div>
+
+  )
+}
+
+export default Testimonial
